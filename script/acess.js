fa5de562d645477bb6f3e2d81efcd0a15a309b8b
gu <gusera4325@gmail.com>
2024-04-02 17:19:02 -0300

    alte

--- script/acess.js
+++ script/acess.js
@@ -77,3 +77,69 @@ function accessibilidadeSob12() {
         interuptor11=true
     }
 }
+
+var interuptor13 = true;
+function accessibilidadeSob123() {
+    var menum12 = document.getElementById('ad13');
+    var menum13 = document.getElementById('divs1');
+    var menum24 = document.getElementById('circulo3')
+    var menum35 = document.getElementById('textCanal');
+    var menum6 = document.getElementById('d1');
+    var menum16 = document.getElementById('d2');
+    var menum26 = document.getElementById('d3');
+    var menum36 = document.getElementById('d4');
+    var menum57 = document.getElementById('textIdioma2');
+    var menum79 = document.getElementById('confiar');
+    
+    if (interuptor11) {
+        menum12.style.height = '100vh';
+
+        menum13.style.display = 'block';
+
+        menum24.style.display = 'block';
+
+        menum35.style.display = 'block';
+
+        menum46.style.display = 'block';
+
+        menum36.style.display = 'block';
+
+        menum26.style.display = 'block';
+
+        menum16.style.display = 'block';
+
+        menum6.style.display = 'block';
+
+        menum57.style.display = 'block';
+
+        menum79.style.display = 'block';
+
+        interuptor11 = false
+
+    }
+    else { 
+        menum12.style.height = '0vh';
+
+        menum13.style.display = 'none';
+
+        menum24.style.display = 'none';
+
+        menum35.style.display = 'none';
+
+        menum46.style.display = 'none';
+
+        menum36.style.display = 'none';
+
+        menum26.style.display = 'none';
+
+        menum16.style.display = 'none';
+
+        menum6.style.display = 'none';
+
+        menum79.style.display = 'none';
+
+
+
+        interuptor11=true
+    }
+}
